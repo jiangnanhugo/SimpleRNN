8ca728a71a131f691ec99002be36ee2bf222ca39
nan jiang <jiangnan.hugo@gmail.com>
2016-03-01 16:37:50 +0800

    rename and format

--- RNN_Tutorial/rnn_numpy.py
+++ RNN_Tutorial/rnn_numpy.py
@@ -1,83 +1,122 @@
-import csv
 import numpy as np
-import theano
-import theano.tensor as T
-import operator
-import nltk
-import itertools
+from util import softmax
 import datetime
+import sys
 
-from rnn import RNN
+class RNN:
+    '''
+    :keyword
+    word_dim: the size of vocabulary.
+    hidden_dim: the size of hidden layer.
+    '''
+    def __init__(self,word_dim,hidden_dim=100,bptt_truncate=4):
+        # Assign instance varibles
+        self.word_dim=word_dim
+        self.hidden_dim=hidden_dim
+        self.bptt_truncate=bptt_truncate
+        # Randomly initialize the network parameters
+        self.U=np.random.uniform(-np.sqrt(1./word_dim),np.sqrt(1./word_dim),(hidden_dim,word_dim))
+        self.V=np.random.uniform(-np.sqrt(1./hidden_dim),np.sqrt(1./hidden_dim),(word_dim,hidden_dim))
+        self.W=np.random.uniform(-np.sqrt(1./hidden_dim),np.sqrt(1./hidden_dim),(hidden_dim,hidden_dim))
 
-vocabulary_size=8000
-unknown_token='UNKOWN_TOKEN'
-sentence_start_token='SENTENCE_START'
-sentence_end_token='SENTENCE_END'
+    def forward_propagation(self,x):
+        # The total number of time steps
+        T=len(x)
+        # During forwoard propagation we save all hidden states in s because need them later.
+        s=np.zeros((T+1,self.hidden_dim))
+        s[-1]=np.zeros(self.hidden_dim)
+        # The outputs at each time step. Again, we them for later.
+        o=np.zeros((T,self.word_dim))
+        # For each time step...
+        for t in np.arange(T):
+            # Note that we are indexing U by x[t]. This is the same as multiplying U with a one-hot vector.
+            s[t]=np.tanh(self.U[:,x[t]]+self.W.dot(s[t-1]))
+            o[t]=softmax(self.V.dot(s[t]))
+        return [o,s]
 
-#  Read the data and append SENTENCE_START and SENTENCE_END tokens
-print "Reading CSV file..."
-with open('reddit.csv') as f:
-    reader=csv.reader(f,skipinitialspace=True)
-    reader.next()
-    # Split full comment into sentences
-    sentences=itertools.chain(*[nltk.sent_tokenize( x[0].decode('utf-8').lower()) for x in reader])
-    # Append SENTENCE_START and SENTENCE_END
-    sentences=['%s %s %s'%(sentence_start_token,x,sentence_end_token) for x in sentences]
+    def predict(self,x):
+        # Perform forward propagation and return index of the highest score
+        o,s=self.forward_propagation(x)
+        return np.argmax(o,axis=1)
 
-print 'Parsed %d sentences.' %(len(sentences))
+    def calculate_total_loss(self,x,y):
+        L=0
+        # For each sentence...
+        for i in np.arange(len(y)):
+            o,s=self.forward_propagation(x[i])
+            # we only care about our prediction of the 'correct' words
+            correct_word_prediction=o[np.arange(len(y[i])),y[i]]
+            # Add to the loss based on how off we were
+            L+=-1*np.sum(np.log(correct_word_prediction))
 
-# tokenized the sentences into words
-print len(sentences)
-tokenized_sentences=[nltk.word_tokenize(sent) for sent in sentences]
-print len(tokenized_sentences)
-# Count the word frequencies
-word_freq=nltk.FreqDist(itertools.chain(*tokenized_sentences))
-print "Found %d unique words tokens." % len(word_freq.items())
+        return L
 
-# Get the most common words and build index_to_word and word_to_index vectors
-vocab=word_freq.most_common(vocabulary_size-1)
-index2word=[x[0] for x in vocab]
-index2word.append(unknown_token)
-word2index=dict([(w,i) for i,w in enumerate(index2word)])
+    def calculate_loss(self,x,y):
+        # Divide the total loss by the number of training examples
+        N=np.sum(len(y_i) for y_i in y)
+        return self.calculate_total_loss(x,y)/N
 
-print "Using vocabulary size %d." % vocabulary_size
-print "The least frequent word in our vocabulary is '%s' and appeared %d times." % (vocab[-1][0],vocab[-1][1])
+    def bptt(self,x,y):
+        T=len(y)
+        # Perform forward propagation
+        o,s=self.forward_propagation(x)
+        # We accumulate the gradients in these variables
+        dLdU=np.zeros(self.U.shape)
+        dLdV=np.zeros(self.V.shape)
+        dLdW=np.zeros(self.W.shape)
+        delta_o=o
+        delta_o[np.arange(len(y)),y]-=1
+        # For each output backwords...
+        for t in np.arange(T)[::-1]:
+            dLdV+=np.outer(delta_o[t],s[t].T)
+            # Initial delta calculation
+            delta_t=self.V.T.dot(delta_o[t])* (1-(s[t]**2))
+            # Backpropagation through time (for at most self.bptt_truncate steps)
+            for bptt_step in np.arange(max(0,t-self.bptt_truncate),t+1)[::-1]:
+                # Print "Backpropagation step t=%d bptt step=%d" %(t,bptt_step)
+                dLdV+=np.outer(delta_t,s[bptt_step-1])
+                dLdU[:,x[bptt_step]]+=delta_t
+                # Update delta for next step
+                delta_t=self.W.T.dot(delta_t)*(1-s[bptt_step-1]**2)
+
+        return [dLdU,dLdV,dLdW]
+
+    def sgd_step(self,x,y,learning_rate):
+        # Calculate the gradients
+        dLdU,dLdV,dLdW=self.bptt(x,y)
+        # Change parameters according to gradients and learning rate
+        self.U -=learning_rate*dLdU
+        self.V -=learning_rate*dLdV
+        self.W -=learning_rate*dLdW
 
 
-# Replace all words not in our vocabulary with the unknown token
-for i,sent in enumerate(tokenized_sentences):
-    tokenized_sentences[i]=[w if w in word2index else unknown_token for w in sent]
 
+# Outer SGD Loop
+# -model: The RNN model instance
+# - X_train: The training dataset
+# - y_train: The training data labels
+# - learning_rate: Initial learning rate for SGD
+# - nepoch: Number of times to iterate through the complete dataset
+# - evaluate_loss_after: Evaluate the loss after this many epoches
+def train_with_sgd(model,X_train,y_train,learning_rate=0.005,nepoch=100,evaluate_loss_after=5):
+    # We keep track of the losses so we can plot them later
+    losses=[]
+    num_examples_seen=0
+    for epoch in range(nepoch):
+        # Optionally evaluate the loss
+        if(epoch % evaluate_loss_after==0):
+            loss=model.calculate_loss(X_train,y_train)
+            losses.append((num_examples_seen,loss))
+            time=datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            print "%s: Loss after num_examples_seen=%d epoch=%d: %f" %(time,num_examples_seen,epoch,loss)
+            # Adjust the learning rate if loss increases
+            if(len(losses)>1 and losses[-1][1] >losses[-2][1]):
+                learning_rate*=0.5
+                print "Setting learning rate to %f" % learning_rate
 
-print "\nExample sentence: '%s'" % sentences[0]
-print "\nExample sentence after Pre-processing: '%s'" % tokenized_sentences[0]
-print len(tokenized_sentences)
-
-# Create the training data
-X_train=np.asarray([[word2index[w] for w in sent[:-1]] for sent in tokenized_sentences])
-y_train=np.asarray([[word2index[w] for w in sent[1:]] for sent in tokenized_sentences])
-
-# Print an training data example
-x_example,y_example=X_train[17],y_train[17]
-print "x:\n%s\n%s" %(" ".join([index2word[x] for x in x_example]),x_example)
-print "\ny:\n%s\n%s" %(" ".join([index2word[x] for x in y_example]),y_example)
-
-# RNN model
-np.random.seed(10)
-model=RNN(vocabulary_size)
-o,s=model.forward_propagation(X_train[10])
-print X_train[10]
-print o.shape
-print o
-
-predictions=model.predict(X_train[10])
-print predictions.shape
-print predictions
-
-# Limit to 1000 examples to save time
-print "Expected Loss for random predicitons: %f " % np.log(vocabulary_size)
-print "Actual loss: %f" % model.calculate_loss(X_train[:1000],y_train[:1000])
-
-np.random.seed(10)
-model = RNN(vocabulary_size)
-model.sgd_step(X_train[10], y_train[10], 0.005)
+            sys.stdout.flush()
+        # For each training example...
+            for i in range(len(y_train)):
+                # One SGD step
+                model.sgd_step(X_train[i],y_train[i],learning_rate)
+                num_examples_seen+=1
---
+++ RNN_Tutorial/train_theano.py
@@ -0,0 +1,86 @@
+import nltk
+import sys
+import os
+import csv
+import time
+import itertools
+from datetime import datetime
+from util import *
+from rnn_theano import RNN
+
+vocabulary_size=8000
+hidden_dim=80
+learning_rate=0.005
+nepoch=100
+sentence_start_token='SENTENCE_START'
+sentence_end_token='SENTENCE_END'
+unknown_token="UNKNOWN_TOKEN"
+model_file=''
+
+def train_with_sgd(model,X_train,y_train,learning_rate=0.005,nepoch=1,evaluate_loss_after=5):
+    # We keep track of lossed so we can plot them later
+    losses=[]
+    num_examples_seen=0
+    for epoch in range(nepoch):
+        # Optionally evaluate the loss
+        if(epoch % evaluate_loss_after==0):
+            loss=model.calculate_loss(X_train,y_train)
+            losses.append((num_examples_seen,loss))
+            time=datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+            print "%s: Loss after num_examples_seen=%d epoch=%d: %f" %(time,num_examples_seen,epoch,loss)
+            # Adjust the learning rate if loss increases
+            if (len(losses)>1 and losses[-1][1]>losses[-2][1]):
+                learning_rate=learning_rate*0.5
+                print "Setting learning rate to %f" % learning_rate
+            sys.stdout.flush()
+            # Saving model parameters
+            save_model_parameters_theano("./data/rnn-theano-%d-%d-%s.npz" %(model.hidden_dim,model.word_dim,time),model)
+        # For each training examples...
+        for i in range(len(y_train)):
+            # One SGD Step
+            model.sgd_step(X_train[i],y_train[i],learning_rate)
+            num_examples_seen+=1
+
+print "Reading CSV file..."
+with open('reddit.csv') as f:
+    reader=csv.reader(f,skipinitialspace=True)
+    reader.next()
+    # Split full comments into sentences
+    sentences=itertools.chain(*[nltk.sent_tokenize(x[0].decode('utf-8').lower()) for x in reader])
+    # Append SENTENCE_START and SENTENCE_END
+    sentences=["%s %s %s" % (sentence_start_token,x,sentence_end_token) for x in sentences]
+print "Parsed %d sentences." %(len(sentences))
+
+# Tokenize the sentences into words
+tokenized_sentences=[nltk.word_tokenize(sent) for sent in sentences]
+
+# Count the word frequencies
+word_freq=nltk.FreqDist(itertools.chain(*tokenized_sentences))
+print "Founded %d unique words tokens." %len(word_freq.items())
+
+# Get the most common words and build index2word and word2index vectors
+vocab=word_freq.most_common(vocabulary_size-1)
+index2word=[x[0] for x in vocab]
+index2word.append(unknown_token)
+word2index=dict([(w,i) for i,w in enumerate(index2word)])
+
+print "using vocabulary size %d." % vocabulary_size
+print "The least frequent word in out vocabulay is '%s' and apeared %d times." %(vocab[-1][0],vocab[-1][1])
+
+# Replace all owrds not in our vocabulary with the unkwnon token
+for i,sent in enumerate(tokenized_sentences):
+    tokenized_sentences[i]=[w if w in word2index else unknown_token for w in sent]
+
+# Create the training data
+X_train=np.asarray([[word2index for w in sent[:-1]] for sent in tokenized_sentences])
+y_train=np.asarray([[word2index for w in sent[1:]] for sent in tokenized_sentences])
+
+
+
+
+model=RNN(vocabulary_size,hidden_dim=hidden_dim)
+t1=time.time()
+model.sgd_step(X_train[10],y_train[10],learning_rate)
+t2=time.time()
+print "SGD Step time: %f milliseconds" %((t2-t1)*1000.)
+train_with_sgd(model,X_train,y_train,nepoch=nepoch,learning_rate=learning_rate)
